cd71950d45d09749d982101f1b1553cc9a5d6433
irshathahamed21 <21irshathkhan@gmail.com>
2022-06-22 16:48:34 +0530

    redux folder created

---
+++ client/src/redux/allProducts/actions.js
@@ -0,0 +1,8 @@
+import { GET_DATA_ERROR , GET_DATA_SUCCESS, GET_DATA_ERROR, SET_CATEGORY,SET_GENDER,SET_GRID,SET_PAGE,SET_SORT, RESET_FILTER} from "./actionTypes";
+import axios from "axios"
+
+
+export const getDataLoading = () => {
+    
+}
+

0ec2d651f1c63f4d88367bd95e6af9ed72d3f6d9
irshathahamed21 <21irshathkhan@gmail.com>
2022-06-24 11:27:55 +0530

    product model created

---
+++ server/src/models/product.model.js
@@ -0,0 +1,23 @@
+const {Schema, model} = require("mongoose")
+
+const reqArray = {type:Array, required:true}
+const reqString = {type:String, required:true}
+const reqNumber = {type:Number, required:true}
+
+const productSchema = Schema({
+    name:reqString,
+    img:reqArray,
+    description:reqString,
+    category:reqString,
+    price:reqNumber,
+    size:reqArray,
+    rating:reqNumber,
+    gender:reqString,
+    brand:reqString,
+    collection:reqString
+}, {
+    versionKey:false
+})
+
+module.exports = model("product", productSchema)
+

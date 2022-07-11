06c34540e75bdc609960c4eb9bf783918097e535
irshathahamed21 <21irshathkhan@gmail.com>
2022-07-11 18:01:40 +0530

    reducer for cart products is created

---
+++ client/src/redux/cartProducts/reducer.js
@@ -0,0 +1,61 @@
+import { ADD_TO_CART_LOADING, ADD_TO_CART_SUCCESS, ADD_TO_CART_ERROR,SET_CART_TOTAL,SET_SHIPPING_ADDRESS,DELETE_FROM_CART_SUCCESS} from "./actionTypes";
+
+
+const initState = {
+    isLoading:false,
+    cart:[],
+    isError:false,
+    amount: {
+        discount: 0,
+        productCount: 0,
+        shippingCharges: 0,
+        totalMRP: 0,
+        payableAmount: 0
+    },
+    address: {
+        fullName: "",
+        mobile: "",
+        streetAddress: "",
+        landmark: "",
+        city: "",
+        state: "",
+        pincode: ""
+    }
+}
+
+export const cartReducer = (state=initState, {type,payload} ) => {
+    switch(type){
+        case ADD_TO_CART_LOADING:
+            return {...state, isLoading:true}
+        case ADD_TO_CART_SUCCESS:
+            return {...state, cart:payload, isError:false}
+        case ADD_TO_CART_ERROR:
+            return { ...state, isLoading: false, isError: true };
+        case SET_CART_TOTAL:
+            return {
+                ...state,
+                amount: {
+                    discount: payload.discount,
+                    productCount: payload.productCount,
+                    shippingCharges: payload.shippingCharges,
+                    totalMRP: payload.totalMRP,
+                    payableAmount: payload.payableAmount
+                }
+            };
+        case SET_SHIPPING_ADDRESS:
+            return {
+                ...state,
+                address: {
+                    fullName: payload.fullName,
+                    mobile: payload.mobile,
+                    streetAddress: payload.streetAddress,
+                    landmark: payload.landmark,
+                    city: payload.city,
+                    state: payload.state,
+                    pincode: payload.pincode
+                }
+            };
+        default:
+            return state;
+    }
+}

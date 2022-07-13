f81804272504aaec8b67f715bd0e35862f1afa94
irshathahamed21 <21irshathkhan@gmail.com>
2022-07-13 16:31:56 +0530

    ProductBox component created

---
+++ client/src/components/products/ProductBox.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Box,Flex,Image,Text} from "@chakra-ui/react"
+import { numberWithCommas, shortString } from "../../utils/extrafunctions";
+import {Link} from "react-router-dom";
+
+
+const ProductBox = ({data}) => {
+  const {_id, img, name, description, price, rating} = data
+
+  return (
+    <>
+    <Link >
+        <Flex flexDirection={"column"} boxShadow= "sm" >
+          <Box>
+            <Image src = {img[0]}/>
+          </Box>
+          <Box>
+            <Text fontWeight={600} fontSize={18} m={"10px 10px 2px"}>{shortString(name,15)}</Text>
+            <Text fontSize={14} color={'grey'} m={"2px 10px 2px"}>{description}</Text>
+            <Text m={"2px 10px 2px"} fontSize={['20px']} color={'red'}>
+            <Text color={'grey'} as='del'>₹{numberWithCommas(price + Math.floor(0.2 * price))}</Text> &nbsp;
+            ₹{numberWithCommas(price)}
+            </Text>
+            <Text fontSize={14} color={'grey'} m={"2px 10px 2px"}>20% OFF</Text>
+            <Text fontSize={14} color={'grey'} m={"1px 10px 2px"}>Rating: {rating}</Text>
+          </Box>
+        </Flex>
+    </Link>
+
+    </>
+  )
+}
+
+export default ProductBox

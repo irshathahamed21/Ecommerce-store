acfbca6cf38bf55be2d8ab2e0b4c456de0d0d1b6
irshathahamed21 <21irshathkhan@gmail.com>
2022-07-13 16:28:36 +0530

    category menu component created

---
+++ client/src/components/products/CategoryMenu.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Menu, MenuButton, MenuList,MenuItem} from "@chakra-ui/react"
+import {useDispatch} from "react-redux"
+import { BsFillCaretDownFill } from "react-icons/bs";
+import { setCategory } from '../../redux/allProducts/actions';
+import { setItem } from '../../utils/sessionStorage';
+
+
+const CategoryMenu = () => {
+    const dispatch = useDispatch()
+
+    const handleCategoryChange = ({target:{value}}) => {
+        dispatch(setCategory(value))
+        setItem("category", value)
+    }
+
+
+
+  return (
+    <Menu>
+  <MenuButton as={Button} rightIcon={<BsFillCaretDownFill />}>
+    Category
+  </MenuButton>
+  <MenuList>
+    <MenuItem onClick = {handleCategoryChange} value = {"allCategory"}>All Categories</MenuItem>
+    <MenuItem onClick = {handleCategoryChange} value = {"cloths"}>Cloths</MenuItem>
+    <MenuItem onClick = {handleCategoryChange} value = {"shoes"}>Shoes</MenuItem>
+    
+  </MenuList>
+</Menu>
+  )
+}
+
+export default CategoryMenu

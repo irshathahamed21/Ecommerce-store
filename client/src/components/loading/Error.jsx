4bc8208ce9ba1a4a7f9fbbf13d0b127469e556e0
irshathahamed21 <21irshathkhan@gmail.com>
2022-07-14 13:40:24 +0530

    Error component created

---
+++ client/src/components/loading/Error.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Container, Image} from "@chakra-ui/react"
+
+
+const Error = () => {
+  return (
+    <>
+    <Container my={['150px', '0px']} maxW={'470px'}>
+         <Image src="images/error.gif" />
+    </Container>
+    </>
+  )
+}
+
+export default Error
